0a8395319d5c6740c81ddb2e00658a52159da10d
Mr-Rem <ormasarem@gmail.com>
2025-03-22 11:00:11 +0800

    Update chat.php

--- chat.php
+++ chat.php
@@ -2,6 +2,15 @@
 session_start();
 include 'db.php'; // Database connection
 
+header("Access-Control-Allow-Origin: *"); // Allow requests from any origin
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 // Redirect if not logged in
 if (!isset($_SESSION["user_id"])) {
     header("Location: login.php");
