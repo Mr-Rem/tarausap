3518926a174527a602aee906f6fb9df8f836ba83
Mr-Rem <ormasarem@gmail.com>
2025-03-22 11:00:32 +0800

    Update db.php

--- db.php
+++ db.php
@@ -9,4 +9,12 @@ $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+header("Access-Control-Allow-Origin: *"); // Allow requests from any origin
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 ?>
